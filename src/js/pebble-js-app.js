6977dec98536259ef19229344c5cf56f4633549b
Jon Barlow <orviwan@gmail.com>
2016-01-14 02:15:11 +0000

    Kinda working

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -3,26 +3,21 @@ Pebble.addEventListener('ready', function() {
 });
 
 Pebble.addEventListener('showConfiguration', function() {
-  var url = 'https://rawgit.com/pebble-examples/design-guides-slate-config/master/config/index.html';
-  console.log('Showing configuration page: ' + url);
-
+  var url = 'http://9ecdf39b.ngrok.io/index.html';
   Pebble.openURL(url);
 });
 
 Pebble.addEventListener('webviewclosed', function(e) {
-  var configData = JSON.parse(decodeURIComponent(e.response));
-  console.log('Configuration page returned: ' + JSON.stringify(configData));
-
-  var backgroundColor = configData['background_color'];
-
+  var data = JSON.parse(decodeURIComponent(e.response));
+  //console.log('Configuration page returned: ' + JSON.stringify(configData));
   var dict = {};
-  if(configData['high_contrast'] === true) {
-    dict['KEY_HIGH_CONTRAST'] = configData['high_contrast'] ? 1 : 0;  // Send a boolean as an integer
-  } else {
-    dict['KEY_COLOR_RED'] = parseInt(backgroundColor.substring(2, 4), 16);
-    dict['KEY_COLOR_GREEN'] = parseInt(backgroundColor.substring(4, 6), 16);
-    dict['KEY_COLOR_BLUE'] = parseInt(backgroundColor.substring(6), 16);
-  }
+  dict['BACKGROUND_KEY'] = parseInt(data['Background'], 16);
+  dict['LOCALTIME_KEY'] = parseInt(data['LocalTime'], 16);
+  dict['REMOTETIME_KEY'] = parseInt(data['RemoteTime'], 16);
+  dict['DAY_KEY'] = parseInt(data['Day'], 16);
+  dict['DATE_KEY'] = parseInt(data['Date'], 16);
+  dict['OFFSET_KEY'] = parseInt(data['Offset']) * 60 * 60;
+  dict['LABEL_KEY'] = data['Label'];
 
   // Send to watchapp
   Pebble.sendAppMessage(dict, function() {
--- src/main.c
+++ src/main.c
@@ -1,80 +1,152 @@
 #include <pebble.h>
+#include "main.h"
 
 static Window *s_window;
-
 static Layer *s_window_layer;
 TextLayer *local_time_layer, *remote_time_layer, *day_layer, *date_layer;
-static time_t offset_seconds = 28800; //8 hours
+static time_t offset_seconds = 0;
+AppSettings settings;
 
-static void handle_tick(struct tm *tick_time, TimeUnits units_changed) {
-	
+void settings_load() {
+	settings.Background = GColorRed;
+	settings.LocalTime = GColorWhite;
+	settings.RemoteTime = GColorLightGray;
+	settings.Day = GColorWhite;
+	settings.Date = GColorWhite;
+	settings.Offset = 0;
+	//settings.Label[] = "";
+	snprintf(settings.Label, sizeof(settings.Label), "YYYY");
+
+  persist_read_data(SETTINGS_KEY, &settings, sizeof(settings));
+}
+
+void settings_save() {
+  persist_write_data(SETTINGS_KEY, &settings, sizeof(settings));
+}
+
+void settings_process_tuple(Tuple *new_tuple) {
+  switch (new_tuple->key) {
+    case BACKGROUND_KEY:
+      settings.Background.argb = GColorFromHEX(new_tuple->value->int32).argb;
+			window_set_background_color(s_window, settings.Background);
+      break;
+		case LOCALTIME_KEY:
+			settings.LocalTime.argb = GColorFromHEX(new_tuple->value->int32).argb;
+			text_layer_set_text_color(local_time_layer, settings.LocalTime);
+			break;
+		case REMOTETIME_KEY:
+			settings.RemoteTime.argb = GColorFromHEX(new_tuple->value->int32).argb;
+			text_layer_set_text_color(remote_time_layer, settings.RemoteTime);
+			break;
+		case DAY_KEY:
+			settings.Day.argb = GColorFromHEX(new_tuple->value->int32).argb;
+			text_layer_set_text_color(day_layer, settings.Day);
+			break;
+		case DATE_KEY:
+			settings.Date.argb = GColorFromHEX(new_tuple->value->int32).argb;
+			text_layer_set_text_color(date_layer, settings.Date);
+			break;
+		case OFFSET_KEY:
+			settings.Offset = new_tuple->value->int32;
+			offset_seconds = settings.Offset;
+			force_tick();
+			break;
+		case LABEL_KEY:
+			snprintf(settings.Label, sizeof(settings.Label), new_tuple->value->cstring);
+			force_tick();
+			break;
+  }
+}
+
+void inbox_received_handler(DictionaryIterator *iter, void *context) {
+	Tuple *t = dict_read_first(iter);
+  while(t != NULL) {
+    if(t) {
+      settings_process_tuple(t);
+    }
+		t = dict_read_next(iter);
+  }
+	settings_save();
+}
+
+void handle_tick(struct tm *tick_time, TimeUnits units_changed) {
   if (units_changed & DAY_UNIT) {
-		setlocale(LC_ALL, ""); 
-		
 		static char s_day_buffer[20];
 		strftime(s_day_buffer, sizeof(s_day_buffer), "%d %a", tick_time);
     text_layer_set_text(day_layer, s_day_buffer);
-		
+
     static char s_date_buffer[20];
 		strftime(s_date_buffer, sizeof(s_date_buffer), "%b %Y", tick_time);
     text_layer_set_text(date_layer, s_date_buffer);
   }
-	
-  if (units_changed & MINUTE_UNIT) { 
+  if (units_changed & MINUTE_UNIT) {
     static char s_time_buffer[] = "00:00";
     strftime(s_time_buffer, sizeof(s_time_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
     text_layer_set_text(local_time_layer, s_time_buffer);
-		
-		static char s_time_buffer2[] = "PST: 00:00";
+
+		static char s_time_buffer2[] = "00:00";
+		static char s_time_buffer3[] = "XXXXX: 00:00";
 		time_t now_seconds = mktime(tick_time);
-		time_t seconds = now_seconds - offset_seconds;
+		struct tm *tm_utc = gmtime(&now_seconds);
+		time_t utc_seconds = mktime(tm_utc);
+		time_t seconds = utc_seconds + offset_seconds;
 		struct tm *tm_remote = localtime(&seconds);
-    strftime(s_time_buffer2, sizeof(s_time_buffer2), clock_is_24h_style() ? "PST: %H:%M" : "PST: %I:%M", tm_remote);
-    text_layer_set_text(remote_time_layer, s_time_buffer2);		
+    strftime(s_time_buffer2, sizeof(s_time_buffer2), clock_is_24h_style() ? "%H:%M" : "%I:%M", tm_remote);
+		snprintf(s_time_buffer3, sizeof(s_time_buffer3), "%s: %s", settings.Label, s_time_buffer2);
+    text_layer_set_text(remote_time_layer, s_time_buffer3);
   }
-	
 }
 
-static void main_window_load(Window *window) {
-	
+void force_tick() {
+	time_t now = time(NULL);
+	struct tm *tick_time = localtime(&now);
+	handle_tick(tick_time, DAY_UNIT|MINUTE_UNIT);
+}
+
+void main_window_load(Window *window) {
+	settings_load();
+
+	window_set_background_color(s_window, settings.Background);
+
+	setlocale(LC_ALL, "");
+
 	remote_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(42, 37), PBL_IF_ROUND_ELSE(181, 145), 40));
-	text_layer_set_text_color(remote_time_layer, GColorLightGray);
+	text_layer_set_text_color(remote_time_layer, settings.RemoteTime);
 	text_layer_set_background_color(remote_time_layer, GColorClear);
 	text_layer_set_font(remote_time_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_IMAGINE_18)));
 	text_layer_set_text_alignment(remote_time_layer, PBL_IF_ROUND_ELSE(GTextAlignmentCenter, GTextAlignmentRight));
 	layer_add_child(s_window_layer, text_layer_get_layer(remote_time_layer));
-	
-  local_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(52, 47), PBL_IF_ROUND_ELSE(184, 147), 55));  
-	text_layer_set_text_color(local_time_layer, GColorWhite); 
+
+  local_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(52, 47), PBL_IF_ROUND_ELSE(184, 147), 55));
+	text_layer_set_text_color(local_time_layer, settings.LocalTime);
 	text_layer_set_background_color(local_time_layer, GColorClear);
 	text_layer_set_font(local_time_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_IMAGINE_48)));
 	text_layer_set_text_alignment(local_time_layer, PBL_IF_ROUND_ELSE(GTextAlignmentCenter, GTextAlignmentRight));
 	layer_add_child(s_window_layer, text_layer_get_layer(local_time_layer));
-	
+
   day_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(101, 96), PBL_IF_ROUND_ELSE(183, 146), 55));
-	text_layer_set_text_color(day_layer, GColorWhite);
+	text_layer_set_text_color(day_layer, settings.Day);
 	text_layer_set_background_color(day_layer, GColorClear);
 	text_layer_set_font(day_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_IMAGINE_24)));
 	text_layer_set_text_alignment(day_layer, PBL_IF_ROUND_ELSE(GTextAlignmentCenter, GTextAlignmentRight));
 	layer_add_child(s_window_layer, text_layer_get_layer(day_layer));
-	
+
   date_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(128, 123), PBL_IF_ROUND_ELSE(181, 145), 20));
-	text_layer_set_text_color(date_layer, GColorWhite);
+	text_layer_set_text_color(date_layer, settings.Date);
 	text_layer_set_background_color(date_layer, GColorClear);
 	text_layer_set_font(date_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_IMAGINE_18)));
 	text_layer_set_text_alignment(date_layer, PBL_IF_ROUND_ELSE(GTextAlignmentCenter, GTextAlignmentRight));
 	layer_add_child(s_window_layer, text_layer_get_layer(date_layer));
 
-	time_t now = time(NULL);
-  struct tm *tick_time = localtime(&now);  
+	force_tick();
 
-  handle_tick(tick_time, DAY_UNIT + MINUTE_UNIT);
   tick_timer_service_subscribe(MINUTE_UNIT, handle_tick);
-	
+
 }
 
-static void main_window_unload(Window *window) {
+void main_window_unload(Window *window) {
 	tick_timer_service_unsubscribe();
+
   text_layer_destroy(local_time_layer);
   text_layer_destroy(day_layer);
   text_layer_destroy(date_layer);
@@ -84,15 +156,17 @@ static void main_window_unload(Window *window) {
 
 void handle_init(void) {
   s_window = window_create();
-	window_set_background_color(s_window, GColorBlack);
 	s_window_layer = window_get_root_layer(s_window);
-	
+
 	window_set_window_handlers(s_window, (WindowHandlers) {
 		.load = main_window_load,
 		.unload = main_window_unload,
 	});
-	
+
   window_stack_push(s_window, true);
+
+	app_message_register_inbox_received(inbox_received_handler);
+	app_message_open(128, 128);
 }
 
 void handle_deinit(void) {
